c276e79dc4c30a77a5524104f3723e44d697fce8
DavidSaupe <david.saupe@gmail.com>
2020-12-01 15:50:33 +0100

    fungerande version

--- app.js
+++ app.js
@@ -1,36 +1,67 @@
 
 
-const addProduct = function(e) {
+const addProduct = function() {
  
-    e.preventDefault;
+    
 
-const productList = []
+
 const name = document.querySelector("#inputText").value;
 const description = document.querySelector("#inputDescription").value;
 const price = document.querySelector("#inputNumber").value;
 
     const addProductBox = document.createElement("div");
     addProductBox.className = "box";
-    const createP1 = document.createElement("p")
-    createP1.innerText = name
-    const addName = document.querySelector(".box").appendChild(createP1)
-    const createP2 = document.createElement("p")
-    createP2.innerText = description
-    const addName2 = document.querySelector(".box").appendChild(createP2)
-    const createP3 = document.createElement("p")
-    createP3.innerText = price
-    const addName3 = document.querySelector(".box").appendChild(createP3)
-    
-    
-    //name + "  " + description + "  " + price + "$";
     const push = document.querySelector("#products").appendChild(addProductBox)
-    productList.push(push)
 
+    
+
+
+    addProductBox.innerText = name + " " + description + " " + price
 
 
 
 
 }
 
+
+
+var checkInputValueName = function () {
+    
+    const name = document.querySelector("#inputText").value;
+
+    if (name === "") {
+        alert("Please add a name to your product!")
+    }
+    else {
+        checkInputValueDescription();
+        
+    }
+}
+var checkInputValueDescription = function () {
+    
+    const description = document.querySelector("#inputDescription").value;
+
+    if (description === "") {
+        alert("Please add a description to your product!")
+    }
+    else {
+        checkInputValuePrice();
+        
+    }
+}
+var checkInputValuePrice = function () {
+ 
+    
+    const price = Number(document.querySelector("#inputNumber").value);
+   
+
+    if (price > 0 ) {
+        addProduct();
+    }
+    else {
+        alert("Please add a price to your product!")
+    }
+}
+
 const btn = document.querySelector("#addBtn");
-btn.addEventListener("click", addProduct);
+btn.addEventListener("click", checkInputValueName);
