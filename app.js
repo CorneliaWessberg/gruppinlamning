a3db09b3d5eb46b14190f0a2b16b38e2277259f8
DavidSaupe <david.saupe@gmail.com>
2020-12-03 14:05:09 +0100

    not totaly functioning

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var lista = []
+var lista2 = []
 const addProduct = function() {
  
     
@@ -21,20 +22,59 @@ const price = document.querySelector("#inputNumber").value;
 
     //creating an img tag as a child to the list item with class name li-Image
 
-    addProductBox.innerHTML = "<li id=\"li-Image" + lista.length +  "\" </li>"  +  ` <li> ${name} </li> <li> ${description} </li> <li> Price: ${price} kr </li>` + "</li>" + "<button id= \" btnAddToCart" + lista.length + "\"  > Add to cart </button>"
+    
 
-    const imgUrl = document.querySelector("#inputImage").value;
+    
+
+
+
+    addProductBox.innerHTML = "<li id=\"limg" + lista.length +  "\" > </li>"  +  ` <li> ${name} </li> <li> ${description} </li> <li> Price: ${price} kr </li>` + "</li>" + "<button id= \" btnAddToCart" + lista.length + "\"  > Add to cart </button>"
+
+    
+
+
+const imgUrl = document.querySelector("#inputImage").value;
     const createImg = document.createElement("img");
     createImg.src = imgUrl
-    document.querySelector("#li-Image").appendChild(createImg)
-
     createImg.className = "imgStyle";
-    
+
     
 
     
-// list to give different id names to the buttons
-lista.push(price)
+    const listaId = "\"#limg" + lista.length + "\"";
+    lista.push(listaId);
+
+    for (var i = 0; i < lista.length; i++) {
+
+        console.log(lista[i])
+        
+    }
+
+  
+    const varId = lista[lista.length - 1]
+    console.log(varId)
+    const didiget = document.querySelector(varId);
+    didiget.textContent = "hej"
+
+    
+
+    
+
+    console.log(didiget)
+
+    console.log(lista)
+
+    console.log (createImg)
+    
+    
+    // const btnId = "btnAddToCart" + lista2[j];
+    // lista2.push(btnId);
+
+    // for (var j = 0; j < lista2.length; j++) {
+
+    //     document.getElementById(lista2[j])
+
+    // }
    
 }
 
@@ -94,9 +134,11 @@ const btnCart = document.querySelector("#goToCart");
 btnCart.addEventListener("click", goToShoppingcart)
 
 
-const addToShoppingcart = function () {
-    console.log("hej")
-}
+// const addToShoppingcart = function () {
+//     console.log("hej")
+// }
 
-const btnAddToCart = document.querySelector( "#btnAddToCart" + lista.length);
-btnAddToCart.addEventListener("click", addToShoppingcart)
+
+
+// const btnAddToCart = document.querySelector( "#btnAddToCart");
+// btnAddToCart.addEventListener("click", addToShoppingcart)
