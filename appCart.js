a0a575141e9fc27a827f49762b0bdf70eb83acdd
NoahGordon12 <noah.gordon@medieinstitutet.se>
2020-12-18 14:21:52 +0100

    andring totalPrice

--- appCart.js
+++ appCart.js
@@ -2,15 +2,21 @@
 // a function that adds the products from the cart localStorage to the cart HTML 
 window.onload = function () {
     let products = JSON.parse(localStorage.getItem("cartProducts"))
-
+    //console.log(products)
     let totalPrice = 0;
 
+    // map loopar igenom arrayen products
     products.map((addedToCart) => {
         const img = addedToCart.addedUrl;
         const name = addedToCart.addedName;
-        //const description = addedToCart.addedDescription;
+        // gör om price till en float istället för en sträng
         const price = parseFloat(addedToCart.addedPrice);
-        debugger 
+
+        // räknar ut totalen, plussar på värdet för varje varv i loopen
+        totalPrice += price
+
+
+
         const addProductBox = document.createElement("div");
         addProductBox.className = "box";
 
@@ -18,18 +24,14 @@ window.onload = function () {
         addProductBox.innerHTML = `<li> <img class="imgStyle" src="${img}"> </li> <li class="name" > ${name} </li> <li class="price"> ${price} </li> <li> <p> Quantity: <input id="exemplar"> </p> <li> <button class="cartBtn" id="btn">  &#128465; </button>`
 
 
-        // adding etra buttons for the customer to be able to remove items from their shoppingcarst
+        // adding extra buttons for the customer to be able to remove items from their shoppingcarst
         const removeItem = document.querySelector("#btn")
 
         removeItem.addEventListener('click', function () {
-            removeItem.parentNode.parentNode.remove();
-
-
+        removeItem.parentNode.parentNode.remove();
+            
         })
-
-
-        ///////////////// Lägg till totalpris från (localstorage CartProducts) //////////////
-
+ 
 
         // function that purchases the products in the cart for the customer and resetting the local storage for the cart
         // also moves the customer to a page with their invoice (reciept for download)
@@ -48,11 +50,15 @@ window.onload = function () {
         const removeBtn = document.getElementById("#btn")
         // removeBtn.addEventListener("click", remove)
 
-        //add price to total
-        parseFloat(totalPrice) += Number(price)
+        
         
 
-    })
+    }) // end of loop/map
+
+
+    // create elment
+    // innerhtml = totalprice
+    console.log(totalPrice)
     const totalNode = document.getElementById("total");
     totalNode.innerHTML = `Total: ${totalPrice}`
 }
