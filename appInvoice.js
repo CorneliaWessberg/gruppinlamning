04f27d491a6db84cd14995b8e0b1abea90e4ccc0
DavidSaupe <david.saupe@gmail.com>
2020-12-30 12:12:27 +0100

    final edition

--- appInvoice.js
+++ appInvoice.js
@@ -1,22 +1,6 @@
 window.onload = function () {
     let products = JSON.parse(localStorage.getItem("cartProducts"))
-/*         if (window.localStorage.length === 0) {   
-        }
-        else {
-            
-        products.map((product) => {
-            const id = product.id;
-            const img = product.addedUrl;
-            const name = product.addedName;
-            const description = product.addedDescription;
-            const price = product.addedPrice;
-            const addProductBox = document.createElement("tr");
-            addProductBox.className = "box";
-            
-            document.querySelector("#products").appendChild(addProductBox)
-            addProductBox.innerHTML = `<td> <img class="imgStyle" src="${img}"> </td> <td class="name" > ${name} </td> <td class="description" > ${description} </td> <td class="price"> ${description} </td>`
-            } )
-        } */
+
 
     let totalPrice = 0;
 
@@ -47,18 +31,7 @@ window.onload = function () {
 
 
 
-/*     // map loopar igenom arrayen products
-    products.map((totalCost) => {
-        // gör om price till en float istället för en sträng
-        const price = parseFloat(totalCost.addedPrice);
-    
-        // räknar ut totalen, plussar på värdet för varje varv i loopen
-        totalPrice += price
-        console.log(totalPrice)
-        const totalNode = document.getElementById("total");
-        totalNode.innerHTML = `Total: ${totalPrice} kr` 
-        
-    }) */ 
+
     
      localStorage.removeItem("cartProducts")
 }
@@ -83,19 +56,6 @@ document.getElementById("download")
         html2pdf().from(invoice).set(opt).save();
     });
 
-
-/*
-git add .
-
-git commit -m "brger"
-
-git pull origin master
-
-git add .
-
-git commit -m "ex"
-
-git push origin master */
 
 
 
